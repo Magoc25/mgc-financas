52369cda2676b35c1ba3e88886398ea28b039f68
Magoc25 <129018821+Magoc25@users.noreply.github.com>
2026-04-26 13:26:31 -0300

    fix: esconder banner no modo standalone + cache sw v2 + fechar salva flag

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mgc-v1';
+const CACHE = 'mgc-v2';
 const SHELL = ['./mgc-financas.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
